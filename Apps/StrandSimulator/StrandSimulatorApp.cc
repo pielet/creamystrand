a3062b09932d92517eefad24604b4507e8d6f548
Tiantian <Tiantian@MININT-5TPR49E.fareast.corp.microsoft.com>
2020-06-06 16:19:07 +0800

    test

--- Apps/StrandSimulator/StrandSimulatorApp.cc
+++ Apps/StrandSimulator/StrandSimulatorApp.cc
@@ -251,12 +251,12 @@ Scalar calcViewRadius( const Vec3d& simCenter )
 
 void centerObject()
 {
-    const Vec3d simCenter = calcSimCenter();
-    controller.setCenterMode( ViewController::CENTER_OBJECT );
-    controller.setViewCenter( simCenter );
+    //const Vec3d simCenter = calcSimCenter();
+    controller.setCenterMode( ViewController::CENTER_WORLD_ORIGIN );
+   // controller.setViewCenter( simCenter );
     
-    const Scalar radius = calcViewRadius( simCenter );
-    controller.setBoundingRadius( radius );
+   // const Scalar radius = calcViewRadius( simCenter );
+    controller.setBoundingRadius(73.1935);
 }
 
 void initCamera()
